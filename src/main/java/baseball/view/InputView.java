5b720a6c9dfdbcd4b5fcb1218a662b1241bf238d
kyeoungchan <wkc1002@naver.com>
2023-10-27 15:52:07 +0900

    fix: 코드 리뷰 참고해서 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,14 +2,18 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.domain.Const.ASK_INPUT_IF_CONTINUE;
+import static baseball.domain.Const.ASK_INPUT_NUMBERS;
+
 public class InputView {
+
     public String inputNumbers() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(ASK_INPUT_NUMBERS);
         return Console.readLine();
     }
 
     public String inputIfContinue() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(ASK_INPUT_IF_CONTINUE);
         return Console.readLine();
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,16 +2,20 @@ package baseball.view;
 
 import baseball.domain.Result;
 
+import static baseball.domain.Const.GAME_OVER_MENTION;
+import static baseball.domain.Const.GAME_START_MENTION;
+
 public class OutputView {
+
     public void startMention() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MENTION);
     }
 
     public void printResult(Result result) {
-        System.out.println(result.getResultStringFormat());
+        System.out.println(result);
     }
 
     public void gameOverMention() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(GAME_OVER_MENTION);
     }
 }
